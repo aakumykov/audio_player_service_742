a1742cacee124153ca177a63994d65e15dc3536e
Андрей Кумыков <aakumykov@yandex.ru>
2021-11-03 10:16:43 +0700

    Добавил в абстрактный класс PlayerState свойство iMusicItem.

--- audio_player_service/src/main/java/com/gitlab/aakumykov/audio_player_service/player_states/StoppedPlayerState.java
+++ audio_player_service/src/main/java/com/gitlab/aakumykov/audio_player_service/player_states/StoppedPlayerState.java
@@ -1,11 +1,17 @@
 package com.gitlab.aakumykov.audio_player_service.player_states;
 
 import com.gitlab.aakumykov.audio_player_service.other.ePlayerMode;
+import com.gitlab.aakumykov.audio_player_service.other.iMusicItem;
 
 public class StoppedPlayerState extends PlayerState {
+
+    public StoppedPlayerState(iMusicItem musicItem) {
+        super(musicItem);
+    }
 
     @Override
     public ePlayerMode getPlayerMode() {
         return ePlayerMode.STOPPED;
     }
+
 }
